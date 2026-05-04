f145e6ac435fbfce1e0c1b2ef52f8d605ee84da0
miliovo7-cpu <miliovo7@gmail.com>
2026-05-04 10:50:58 +0800

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,120 @@
+﻿const generatedBibEntries = {
+    "Bashar2016": {
+        "author": "Bashar, Farhan and El-Sakka, Mahmoud R.",
+        "booktitle": "Proceedings of the 11th Joint Conference on Computer Vision, Imaging and Computer Graphics Theory and Applications",
+        "doi": "10.5220/0005698205390547",
+        "keywords": "type:BM3D Improvement, Learning-based, Adaptive Thresholding",
+        "title": "BM3D Image Denoising using Learning-based Adaptive Hard Thresholding",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+    "Buades2005a": {
+        "author": "Buades, Antoni and Coll, Bartomeu and Morel, Jean-Michel",
+        "booktitle": "2005 IEEE Computer Society Conference on Computer Vision and Pattern Recognition",
+        "doi": "10.1109/CVPR.2005.38",
+        "keywords": "type:Foundational Method, NLM, Non-local Means",
+        "title": "A non-local algorithm for image denoising",
+        "type": "inproceedings",
+        "year": "2005"
+    },
+    "Buades2005b": {
+        "author": "Buades, Antoni and Coll, Bartomeu and Morel, Jean-Michel",
+        "doi": "10.1137/040616024",
+        "journal": "Multiscale Modeling and Simulation",
+        "keywords": "type:Review Paper, Survey, Denoising",
+        "number": "2",
+        "pages": "490-530",
+        "title": "A review of image denoising algorithms, with a new one",
+        "type": "article",
+        "volume": "4",
+        "year": "2005"
+    },
+    "Dabov2007": {
+        "author": "Dabov, Kostadin and Foi, Alessandro and Katkovnik, Vladimir and Egiazarian, Karen",
+        "doi": "10.1109/TIP.2007.901238",
+        "journal": "IEEE Transactions on Image Processing",
+        "keywords": "type:Foundational Method, BM3D, Collaborative Filtering",
+        "number": "8",
+        "pages": "2080-2095",
+        "title": "Image denoising by sparse 3-D transform-domain collaborative filtering",
+        "type": "article",
+        "volume": "16",
+        "year": "2007"
+    },
+    "Halfaoui2015": {
+        "author": "Halfaoui, Ibrahim and Urfalioglu, Onay",
+        "booktitle": "2015 IEEE International Conference on Image Processing",
+        "doi": "10.1109/ICIP.2015.7351372",
+        "keywords": "type:BM3D Improvement, NBM3D, Non-stationary Gaussian",
+        "title": "Improving BM3D on non-stationary Gaussian models for real image noise",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "Hasan2018": {
+        "author": "Hasan, Mahmud and El-Sakka, Mahmoud R.",
+        "doi": "10.1186/s13640-018-0264-z",
+        "journal": "EURASIP Journal on Image and Video Processing",
+        "keywords": "type:BM3D Improvement, SSIM, Wiener Filter",
+        "number": "1",
+        "pages": "25",
+        "title": "Improved BM3D image denoising using SSIM-optimized Wiener filter",
+        "type": "article",
+        "volume": "2018",
+        "year": "2018"
+    },
+    "Hou2018": {
+        "author": "Hou, Yingkun and Shen, Dinggang",
+        "doi": "10.1186/s13640-018-0299-1",
+        "journal": "EURASIP Journal on Image and Video Processing",
+        "keywords": "type:BM3D Improvement, Morphological Component, Size-adaptive",
+        "number": "1",
+        "pages": "59",
+        "title": "Image denoising with morphology- and size-adaptive block-matching transform domain filtering",
+        "type": "article",
+        "volume": "2018",
+        "year": "2018"
+    },
+    "Ri2022": {
+        "author": "Ri, Gwang-Il and Kim, Song-Jun and Kim, Man-Su",
+        "doi": "10.1007/s11042-022-12318-z",
+        "journal": "Multimedia Tools and Applications",
+        "keywords": "type:BM3D Improvement, KL Divergence, Multi-scale",
+        "pages": "12661-12679",
+        "title": "Improved BM3D method with modified block-matching and multi-scaled images",
+        "type": "article",
+        "volume": "81",
+        "year": "2022"
+    },
+    "Shao2014": {
+        "author": "Shao, Ling and Yan, Ruomei and Li, Xuelong and Liu, Yan",
+        "doi": "10.1109/TCYB.2013.2278548",
+        "journal": "IEEE Transactions on Cybernetics",
+        "keywords": "type:Review Paper, Survey, Dictionary Learning",
+        "number": "7",
+        "pages": "1001-1013",
+        "title": "From heuristic optimization to dictionary learning: A review and comprehensive comparison of image denoising algorithms",
+        "type": "article",
+        "volume": "44",
+        "year": "2014"
+    },
+    "Song2019": {
+        "author": "Song, Bing and Duan, Zhansheng and Gao, Yongxin and Shao, Teng",
+        "booktitle": "2019 22th International Conference on Information Fusion",
+        "doi": "10.23919/FUSION43075.2019.9012679",
+        "keywords": "type:BM3D Improvement, Coefficient of Variation, Size-adaptive",
+        "title": "Adaptive BM3D Algorithm for Image Denoising Using Coefficient of Variation",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Yahya2020": {
+        "author": "Yahya, Ali Abdullah and Tan, Jieqing and Su, Benyue and Hu, Min and Wang, Yibin and Liu, Kui and Hadi, Ali Naser",
+        "doi": "10.1007/s11042-020-08815-8",
+        "journal": "Multimedia Tools and Applications",
+        "keywords": "type:BM3D Improvement, Adaptive Filtering, Total Variation, K-means",
+        "pages": "20391-20427",
+        "title": "BM3D image denoising algorithm based on an adaptive filtering",
+        "type": "article",
+        "volume": "79",
+        "year": "2020"
+    }
+};
